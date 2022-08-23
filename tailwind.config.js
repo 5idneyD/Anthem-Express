93c9f37d2de5a3ff8e1fe8cda9284cc169ac57c7
5idneyD <sldriscoll@btinternet.com>
2022-08-23 21:01:06 +0100

    Starting new webapp; Anthem Express; Has National Anthems

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'burgundy': '#712E1E'
+      },
+    },
+  }
