bf82eabf6bea7056c87f66e49deee2ca91fa607a
5idneyD <sldriscoll@btinternet.com>
2022-08-27 12:36:44 +0100

    added 3 node_modules required; nock, aws-sdk, mock-aws-s3

--- src/routes/[country]/+page.mjs
+++ src/routes/[country]/+page.mjs
@@ -1,27 +1,11 @@
 import { error } from '@sveltejs/kit';
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-
-const anthem = await prisma.anthem.create({
-  data: {
-    country: "England",
-    text: "God Save the Queen"
-  },
-})
 
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
   if (params.country === 'England') {
 
-    const anthemByCountry = await prisma.anthem.findUnique({
-      where : {
-        country: params.country,
-      },
-    })
-
-    console.log(anthemByCountry.text);
     return {
       title: 'Hello world!',
       content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
--- src/routes/[country]/+page.svelte
+++ src/routes/[country]/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to About</h1>
 
+<script>
+    const sqlite3 = require('sqlite3').verbose();
+</script>
+
