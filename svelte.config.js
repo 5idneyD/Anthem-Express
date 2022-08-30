66aefbf2436528b1a8babd7ad54bae640ebc3989
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-08-30 10:51:09 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,6 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		target: "#svelte",
-		prerender: {
-			crawl: true,
-			enabled: true,
-			onError: 'continue',
-			pages: ["*"]
-		},
 		adapter: adapter(),
 		browser: {
 			hydrate: false,
