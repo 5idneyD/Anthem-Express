63b28e53db379dc7b1cee772d9c99468468a5641
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-08-30 10:56:40 +0100

    Update svelte.config.cjs

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,16 +1,51 @@
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+// 		browser: {
+// 			hydrate: false,
+// 			router: false
+			
+// 		}
+// 	}
+
+// };
+
+// export default config;
+
+
+const sveltePreprocess = require('svelte-preprocess')
+const netlify = require('@sveltejs/adapter-netlify')
+const pkg = require('./package.json')
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(),
-		browser: {
-			hydrate: false,
-			router: false
-			
-		}
-	}
+module.exports = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+-    adapter: node(),
++    adapter: netlify(),
 
-};
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
 
-export default config;
+    vite: {
+      ssr: {
+        noExternal: Object.keys(pkg.dependencies || {}),
+      },
+    },
+  },
+}
+
+
+
+
+
+
+
