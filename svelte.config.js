8df283b4365364ca2e2ffd2bc59808f6b383a459
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-08-30 11:28:39 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: true
+		}),
 		browser: {
 			hydrate: false,
 			router: false
