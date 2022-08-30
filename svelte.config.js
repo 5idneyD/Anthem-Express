5e5d9a19dc2c1b7672cba6d550db5fe4158e5008
5idneyD <84292477+5idneyD@users.noreply.github.com>
2022-08-30 11:06:10 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,39 +1,21 @@
-// import adapter from '@sveltejs/adapter-netlify';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter(),
-// 		browser: {
-// 			hydrate: false,
-// 			router: false
-			
-// 		}
-// 	}
-
-// };
-
-// export default config;
-
-
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: preprocess(),
-    kit: {
-        adapter: adapter(),
-        prerender: {
-            default: true
-        },
-        files: {
-            assets: 'static',
-            lib: 'src/lib'
-        }
-    }
+	kit: {
+		adapter: adapter(),
+		browser: {
+			hydrate: false,
+			router: false
+			
+		}
+	}
+
 };
 
 export default config;
+
+
 
 
 
